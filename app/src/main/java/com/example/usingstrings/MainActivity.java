dd8155ea8e7c7eac0da3ca275ba3dade4a4a7d56
Isuru boy <isurunirash@gmail.com>
2020-08-03 18:34:10 +0530

    Initial Commit

--- app/src/main/java/com/example/usingstrings/MainActivity.java
+++ app/src/main/java/com/example/usingstrings/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.usingstrings;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,5 +18,42 @@ public class MainActivity extends AppCompatActivity {
         textView2 = findViewById(R.id. textView2);
         textView2.setText(R.string.Msg2);
 
+        Log.i( "Lifecycle" , "onCreate() Invoked");
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.i( "Lifecycle" , "onStart() Invoked");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.i( "Lifecycle" , "onResume() Invoked");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.i( "Lifecycle" , "onPause() Invoked");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.i( "Lifecycle" , "onStop() Invoked");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.i( "Lifecycle" , "onDestroy() Invoked");
     }
 }
